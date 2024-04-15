71d7a45a5a8640b4110222b8b7f308918c56f162
rohandarade <rohandarade29@gmail.com>
2024-04-15 15:37:32 +0530

    producer done

--- backend/producer.py
+++ backend/producer.py
@@ -1,13 +1,15 @@
 import random
 import time
+from kafka import KafkaProducer
 
-# List of ticker symbols
+
 ticker_symbols = [
-    "AAPL", "MSFT", "AMZN", "GOOGL", "TSLA", "FB", "NVDA", "PYPL", "ADBE", "INTC",
-    "CMCSA", "NFLX", "CSCO", "PEP", "ABNB", "QCOM", "TMUS", "AVGO", "TXN", "CHTR",
-    "AMD", "SBUX", "AMGN", "COST", "MCD", "GILD", "BKNG", "ADP", "MDLZ", "INTU",
-    "ISRG", "ZM", "VRTX", "REGN", "ATVI", "WBA", "MU", "CSX", "ILMN", "ADI",
-    "IDXX", "DXCM", "MRNA", "KLAC", "LULU", "EBAY", "EA", "NTES", "EXC"
+    "AAPL", "MSFT"
+    #, "AMZN", "GOOGL", "TSLA", "FB", "NVDA", "PYPL", "ADBE", "INTC",
+    # "CMCSA", "NFLX", "CSCO", "PEP", "ABNB", "QCOM", "TMUS", "AVGO", "TXN", "CHTR",
+    # "AMD", "SBUX", "AMGN", "COST", "MCD", "GILD", "BKNG", "ADP", "MDLZ", "INTU",
+    # "ISRG", "ZM", "VRTX", "REGN", "ATVI", "WBA", "MU", "CSX", "ILMN", "ADI",
+    # "IDXX", "DXCM", "MRNA", "KLAC", "LULU", "EBAY", "EA", "NTES", "EXC"
 ]
 
 # Default prices for each ticker symbol
@@ -15,6 +17,12 @@ default_prices = {symbol: round(random.uniform(50, 2000), 2) for symbol in ticke
 
 # Initialize prices with default prices
 prices = default_prices.copy()
+
+# Kafka broker address
+broker_address = 'localhost:9092'
+
+# Create Kafka producer for each ticker symbol
+producers = {symbol: KafkaProducer(bootstrap_servers=broker_address) for symbol in ticker_symbols}
 
 # Function to update prices
 def update_prices():
@@ -24,10 +32,12 @@ def update_prices():
         # Ensure prices don't go below 0
         prices[symbol] = max(prices[symbol], 0)
 
-# Main loop to update and print prices every second
+# Main loop to update and push prices to Kafka every time they change
 while True:
     update_prices()
     for symbol, price in prices.items():
-        print(f"{symbol}: ${price:.2f}", end=", ")
-    print("\n")
-    time.sleep(1)
+        # Push the new price to the corresponding topic
+        topic = symbol.lower()
+        producers[symbol].send(topic, str(price).encode('utf-8'))
+        print(f"Sent: {symbol}: ${price:.2f} to topic: {topic}")
+    time.sleep(1)  # Sleep for 1 second
